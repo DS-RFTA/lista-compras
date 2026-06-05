7cd20d2b036891e88668c3e41e145bdf9d566e1c
Renata Félix Trajano de Araújo <rfta@cesar.school>
2026-06-04 22:58:54 -0300

    Initialize main.py for smart shopping list app

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Lista de Compras Inteligente", page_icon=":sparkles:", layout="centered")
+
+st.title("Lista de Compras Inteligente :sparkles:")
+st.write("Bem-vindo à Lista de Compras Inteligente! Aqui você pode criar e gerenciar suas listas de compras de forma fácil e eficiente. Adicione itens e programe suas compras. Vamos começar a criar sua lista de compras perfeita!")
+
+st.markdown("### Adicione um item à sua lista de compras")
+
